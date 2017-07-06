50bea022d773d3c9432d18505f756b3538ef71ac
tenglai <13049322087@163.com>
2017-07-06 16:43:21 +0800

    a react-native app

--- src/app/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app.module'; // 引入模块文件
-
-// 动态引导
-platformBrowserDynamic().bootstrapModule(AppModule);
