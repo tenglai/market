e6f4b84c77d3a753ef3ba0477bf47423c8cd28b6
tenglai <13049322087@163.com>
2017-08-08 16:46:52 +0800

    add NewsDemo

--- GD/app/main/GDLaunchPage.js
+++ GD/app/main/GDLaunchPage.js
@@ -1,10 +1,11 @@
 /**
  * Created by yeshaojian on 2017/3/25.
  */
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     StyleSheet,
     Image,
+    View,
     Dimensions,
 } from 'react-native';
 
@@ -18,24 +19,35 @@ export default class GDLaunchPage extends Component {
 
     // 组件加载完成
     componentDidMount() {
+        // 倒计时操作
         setTimeout(() => {
             this.props.navigator.replace({
-                component:Main
+                component: Main
             })
         }, 1500)
     }
 
     render() {
-        return(
-            // 启动页
-            <Image source={{uri:'launchimage'}} style={styles.imageStyle} />
+        return (
+            // 启动页，图片资源保存在 android-->app-->src-->main-->res-->drawable-xxhdpi 中
+            // <Image source={{uri: 'launchimage'}} style={styles.imageStyle}/>
+
+            // 也可以使用view,自定义布局文件，也可以组件化的方式引入
+            <View style={styles.container}>
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    container: { // 样式
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#00FF00',
+    },
     imageStyle: {
-        width:width,
-        height:height,
+        width: width,
+        height: height,
     }
 });
---
+++ GD/app/main/HomePage.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Administrator on 2017/6/6.
+ */
+
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Image,
+    Dimensions,
+} from 'react-native';
+
+// 获取屏幕尺寸
+const {width, height} = Dimensions.get('window');
+
+export default class HomePage extends Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit index.android.js
+                </Text>
+                <Text style={styles.instructions}>
+                    Double tap R on your keyboard to reload,{'\n'}
+                    Shake or press menu button for dev menu
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
