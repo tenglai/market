b95788a8743d92d2db31e0c363c3511a0c6f5600
tenglai <13049322087@163.com>
2017-08-11 14:06:03 +0800

    add XMGBuy

--- GD/index.android.js
+++ GD/index.android.js
@@ -6,29 +6,48 @@
 
 import React, { Component } from 'react';
 import {
-    AppRegistry,
-    Navigator,
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
 } from 'react-native';
-
-// 引入外部文件
-import LaunchPage from './app/main/GDLaunchPage';
 
 export default class GD extends Component {
   render() {
     return (
-        <Navigator
-            initialRoute={{
-                name:'launchPage',
-                component:LaunchPage
-            }}
-
-            renderScene={(route, navigator) => {
-                let Component = route.component;
-                return <Component {...route.params} navigator={navigator} />
-            }}
-        />
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
 AppRegistry.registerComponent('GD', () => GD);
